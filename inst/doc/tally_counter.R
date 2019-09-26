ed70c94cd4cf48991fa380ba63991b779bd451be
Graham French <Graham.French@jncc.gov.uk>
2019-09-26 15:59:37 +0100

    Updated vignette

--- inst/doc/tally_counter.R
+++ inst/doc/tally_counter.R
@@ -1,45 +1,43 @@
 ## ---- echo = FALSE-------------------------------------------------------
-library("magrittr")
-library("purrrlyr")
+library("purrr")
 library("tallyr")
 
 ## ---- eval = FALSE-------------------------------------------------------
-#  # install.packages("devtools")
-#  install_github("gcfrench/tallyr")
+#  # install.packages("remotes")
+#  remotes::install_github("gcfrench/tallyr")
 
 ## ------------------------------------------------------------------------
-iris_five <- iris[1:5, ]
-output <- iris_five %>% 
-  by_row(
-  function(x) {
-    message(paste0("The sepal length is ", as.numeric(x[, "Sepal.Length"])), " cm")
-    Sys.sleep(0.25)
-  })
+output <- iris[1:5, ] %>%
+  pmap(
+    function(...) {
+      message(paste0("The sepal length is ", pluck(list(...), "Sepal.Length")), " cm")
+      Sys.sleep(0.25)
+    })
 
 ## ------------------------------------------------------------------------
-output <- iris_five %>% 
-  tally_counter() %>% 
-  by_row(
-  function(x) {
-    message(paste0("The sepal length is ", as.numeric(x[, "Sepal.Length"])), " cm")
-    Sys.sleep(0.25)
-  })
+output <- iris[1:5, ] %>%
+  tally_counter() %>%
+  pmap(
+    function(...) {
+      message(paste0("The sepal length is ", pluck(list(...), "Sepal.Length")), " cm")
+      Sys.sleep(0.25)
+    })
 
 ## ------------------------------------------------------------------------
-output <- iris_five %>% 
-  tally_counter() %>% 
-  by_row(
-    function(x) {
-    message(paste0(click(), ": The sepal length is ", as.numeric(x[, "Sepal.Length"])), " cm")
-    Sys.sleep(0.25)
-  })
+output <- iris[1:5, ] %>%
+  tally_counter() %>%
+  pmap(
+    function(...) {
+      message(paste0(click(), ": The sepal length is ", pluck(list(...), "Sepal.Length")), " cm")
+      Sys.sleep(0.25)
+    })
 
 ## ------------------------------------------------------------------------
-output <- iris_five %>% 
-  tally_counter(type = "add") %>% 
-  by_row(
-  function(x) {
-    message(paste0(click(), ": The sepal length is ", as.numeric(x[, "Sepal.Length"])), " cm")
-    Sys.sleep(0.25)
-  })
+output <- iris[1:5, ] %>%
+  tally_counter(type = "add") %>%
+  pmap(
+    function(...) {
+      message(paste0(click(), ": The sepal length is ", pluck(list(...), "Sepal.Length")), " cm")
+      Sys.sleep(0.25)
+    })
 
