9d979bed27024d6500060db94a6f2830f90f246f
Graham French <Graham.French@jncc.gov.uk>
2019-09-27 16:21:19 +0100

    update hexsticker

--- man/hex_sticker.R
+++ man/hex_sticker.R
@@ -1,5 +1,5 @@
 library(hexSticker)
 sticker(subplot = "man/figures/tally_counter.png", s_x = 1, s_y = 1.15, s_width = 0.54, s_height = 0.675,
-        package = "tallyR", p_x = 1, p_y = 0.4, p_size = 20, p_color = "black",
+        package = "tallyr", p_x = 1, p_y = 0.4, p_size = 20, p_color = "black",
         h_fill = "white", h_color = "black",
         filename = "man/figures/imgfile.png")
