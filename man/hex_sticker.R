dc375ea35b3fa0fc9c64e27462cf1044d2dafd8c
Graham French <Graham.French@jncc.gov.uk>
2019-10-07 17:09:07 +0100

    minor adjustment

--- man/hex_sticker.R
+++ man/hex_sticker.R
@@ -1,6 +1,6 @@
 library(hexSticker)
-sticker(subplot = "./man/figures/tally_counter_hand.png", s_x = 1, s_y = 1.1, s_width = 0.4, s_height = 0.48,
-        package = "tallyr", p_x = 1.05, p_y = 0.4, p_size = 20, p_color = "white",
+sticker(subplot = "./man/figures/tally_counter_hand.png", s_x = 0.95, s_y = 1.1, s_width = 0.4, s_height = 0.48,
+        package = "tallyr", p_x = 1, p_y = 0.4, p_size = 20, p_color = "white",
         h_fill = "#607B8B", h_color = "#0B334D",
         filename = "man/figures/hex_sticker.png")
 
